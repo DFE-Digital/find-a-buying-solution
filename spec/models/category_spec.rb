57e6a4e5ed733c47c67619bb9b6b41228c48f181
Hrishi Mittal <hrishi.mittal@education.gov.uk>
2025-02-06 19:58:30 +0000

    Move VCR config to support file. Refactor and update category specs.

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,13 +1,6 @@
 require "rails_helper"
 
 RSpec.describe Category, type: :model do
-  before do
-    VCR.configure do |config|
-      config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
-      config.hook_into :webmock
-    end
-  end
-
   describe ".all" do
     it "fetches categories from Contentful" do
       VCR.use_cassette("contentful/categories_all") do
--- spec/requests/categories_spec.rb
+++ spec/requests/categories_spec.rb
@@ -1,26 +1,33 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe "Categories page (homepage)", type: :request do
- describe "GET /" do
-   it "includes buying options section heading" do
-     get root_path
-     
-     expect(response).to have_http_status(:success)
-     expect(response.body).to include("Buying options, by category")
-   end
+  around do |example|
+    VCR.use_cassette("contentful/categories_all") { example.run }
+  end
 
-   it "displays all categories from Contentful" do
-     get root_path
+  describe "GET /" do
+    before do
+      get root_path
+    end
 
-     expect(response).to have_http_status(:success)
-     expect(response.body).to include("Vehicle hire and purchase")
-   end
+    it "includes buying options section heading" do
+      expect(response.body).to include("Buying options, by category")
+    end
 
-   it "displays category descriptions" do
-     get root_path
+    it "displays category titles" do
+      expected_titles = ["Vehicle hire and purchase", "Furniture", "ICT and computer software"]
 
-     expect(response).to have_http_status(:success)
-     expect(response.body).to include("Long-term catering contracts")
-   end
- end
+      expected_titles.each do |title|
+        expect(response.body).to include(title)
+      end
+    end
+
+    it "displays category summaries" do
+      expected_summaries = ["Long-term catering contracts", "IT experts and technical advisors", "Lending or buying minibuses, coaches for school trips or other school transport"]
+
+      expected_summaries.each do |summary|
+        expect(response.body).to include(summary)
+      end
+    end
+  end
 end
