7fc260a55c18f16eb7413ff42257fa8fbfdcc51f
Hrishi Mittal <hrishi.mittal@education.gov.uk>
2025-02-06 13:41:28 +0000

    Remove commented ActiveModel includes. Can add later when needed.

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,9 +1,4 @@
 class Category
-  # include ActiveModel::Model
-  # include ActiveModel::Conversion
-  # include ActiveModel::Serialization
-  # include ActiveModel::Naming
-
   attr_reader :id, :title, :summary, :description, :slug
 
   def self.all
