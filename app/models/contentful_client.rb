d2f296da767f70ae154218c5d706edb81268c798
Paul Battley <pbattley@gmail.com>
2025-02-10 17:42:01 +0000

    Make Contentful access lazy to work with VCR
    VCR can't block connections made in initializers.

---
+++ app/models/contentful_client.rb
@@ -0,0 +1,26 @@
+require "singleton"
+
+class ContentfulClient
+  include Singleton
+
+  def self.configure(**kwargs)
+    instance.configure(**kwargs)
+  end
+
+  def self.entries(*args, **kwargs)
+    instance.client.entries(*args, **kwargs)
+  end
+
+  def configure(space:, access_token:)
+    @space = space
+    @access_token = access_token
+  end
+
+  def client
+    @client ||= Contentful::Client.new(
+      space: @space,
+      access_token: @access_token,
+      dynamic_entries: :auto
+    )
+  end
+end
--- config/initializers/contentful.rb
+++ config/initializers/contentful.rb
@@ -1,5 +1,6 @@
-ContentfulClient = Contentful::Client.new(
-  space: ENV.fetch("CONTENTFUL_SPACE_ID", "FAKE_SPACE_ID"),
-  access_token: ENV.fetch("CONTENTFUL_ACCESS_TOKEN", "<API_KEY>"),
-  dynamic_entries: :auto,
-)
+Rails.configuration.after_initialize do
+  ContentfulClient.configure(
+    space: ENV.fetch("CONTENTFUL_SPACE_ID", "FAKE_SPACE_ID"),
+    access_token: ENV.fetch("CONTENTFUL_ACCESS_TOKEN", "FAKE_API_KEY"),
+  )
+end
