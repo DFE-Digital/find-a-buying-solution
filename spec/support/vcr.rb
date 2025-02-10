8b68fa246d5b7564bb038db6e47036e8b3d9efd6
hrmtl <hrishi.mittal@education.gov.uk>
2025-02-10 18:28:10 +0000

    Update VCR

--- spec/support/vcr.rb
+++ spec/support/vcr.rb
@@ -1,6 +1,6 @@
 VCR.configure do |config|
   config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
   config.hook_into :webmock
-  config.filter_sensitive_data("API_KEY") { ENV["CONTENTFUL_ACCESS_TOKEN"] }
+  config.filter_sensitive_data("FAKE_API_KEY") { ENV["CONTENTFUL_ACCESS_TOKEN"] }
   config.filter_sensitive_data("FAKE_SPACE_ID") { ENV["CONTENTFUL_SPACE_ID"] }
 end
