03c23e6b85785662ca8a7e69d24461f7ba04f7d3
hrmtl <hrishi.mittal@education.gov.uk>
2025-02-10 10:16:45 +0000

    Filter out Authorization header from VCR cassette files

--- spec/support/vcr.rb
+++ spec/support/vcr.rb
@@ -1,4 +1,8 @@
 VCR.configure do |config|
   config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
   config.hook_into :webmock
+
+  config.filter_sensitive_data("<API_KEY>") do |interaction|
+    interaction.request.headers["Authorization"]&.first
+  end
 end
