669ef9b4435f4997b967821ce8bfe129c44c5c95
hrmtl <hrishi.mittal@education.gov.uk>
2025-02-11 17:34:14 +0000

    Add request specs for category show page. Update VCR cassettes.

--- spec/support/vcr.rb
+++ spec/support/vcr.rb
@@ -3,4 +3,8 @@ VCR.configure do |config|
   config.hook_into :webmock
   config.filter_sensitive_data("FAKE_API_KEY") { ENV["CONTENTFUL_ACCESS_TOKEN"] }
   config.filter_sensitive_data("FAKE_SPACE_ID") { ENV["CONTENTFUL_SPACE_ID"] }
+  config.default_cassette_options = {
+    match_requests_on: [:method, :uri, :body], 
+    record: :once
+  }
 end
