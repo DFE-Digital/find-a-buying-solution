e89201f31f9532e96f2ff924e09671247f4c50eb
Bhavana <bhavana.kaushik@gmail.com>
2025-02-12 14:06:13 +0000

    - Rectified linting error

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   get "home/index"
   root "categories#index"
-  #root "home#index"
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
