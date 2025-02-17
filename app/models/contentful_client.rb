9d26601a17bf655b66d3dc61a652e0ff12323cbd
Hrishi Mittal <hrishi.mittal@education.gov.uk>
2025-02-17 14:03:55 +0000

    Use to_prepare to make sure Contentful client config reloads in development mode on code changes.

--- app/models/contentful_client.rb
+++ app/models/contentful_client.rb
@@ -12,20 +12,12 @@ class ContentfulClient
   end
 
   def configure(space:, access_token:)
-    return if @space && @access_token
-
     @space = space
     @access_token = access_token
+    @client = nil
   end
 
   def client
-    unless @space && @access_token
-      configure(
-        space: ENV.fetch("CONTENTFUL_SPACE_ID", "FAKE_SPACE_ID"),
-        access_token: ENV.fetch("CONTENTFUL_ACCESS_TOKEN", "FAKE_API_KEY"),
-      )
-    end
-
     @client ||= Contentful::Client.new(
       space: @space,
       access_token: @access_token,
