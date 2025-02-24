8de01203c82544a617f636db77b94ba6e9076537
Bhavana <bhavana.kaushik@gmail.com>
2025-02-24 12:21:27 +0000

    - Created 'Homepage' and 'other' layouts. - Refactored some files into shared instead.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  layout :determine_layout
+
+  private
+  def determine_layout
+    controller_name == 'categories' && action_name == 'index' ? 'homepage' : 'other'
+  end
 end
