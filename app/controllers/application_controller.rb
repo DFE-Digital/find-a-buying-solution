bb5b6617506200aafa11f91331815cfce1b6d5ec
Bhavana <bhavana.kaushik@gmail.com>
2025-03-05 17:41:00 +0000

    - Disabled search on top right corner of the header only for Homepage

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,16 @@
 class ApplicationController < ActionController::Base
   default_form_builder GOVUKDesignSystemFormBuilder::FormBuilder
   rescue_from ContentfulRecordNotFoundError, with: :record_not_found
+  before_action :enable_search_in_header
 
 private
 
   def record_not_found(exception)
     @message = exception.message || "Record not found"
     render template: "errors/not_found", status: :not_found
+  end
+
+  def enable_search_in_header
+    @show_search_in_header = true
   end
 end
