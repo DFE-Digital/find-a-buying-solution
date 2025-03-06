bff1045d706af1f944b425e46d66411245f462db
Bhavana <bhavana.kaushik@gmail.com>
2025-03-06 10:58:52 +0000

    - Merged app/javascript into app/assets/javascripts into one application.js file - Made the relavent refactoring changes to the path in package.json file

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,1 +1,4 @@
 import 'dfe-frontend/packages/dfefrontend';
+
+import * as govukFrontend from 'govuk-frontend';
+govukFrontend.initAll();
--- app/javascript/application.js
+++
@@ -1,2 +0,0 @@
-import * as govukFrontend from 'govuk-frontend';
-govukFrontend.initAll();
