d768396e951a237eff48a104ef8157e64ce5d159
hrmtl <hrishi.mittal@education.gov.uk>
2025-03-10 09:46:59 +0000

    Remove commented code

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,9 +2,6 @@ class SearchController < ApplicationController
   before_action :disable_search_in_header
 
   def index
-    # @search_results = ContentfulClient.entries(
-    #   'query': params[:query]
-    # ).select { |entry| %w[solution category].include?(entry.content_type.id) }
     @solutions = Solution.search(query: params[:query])
     @categories = Category.search(query: params[:query])
     @show_search_in_header = false
