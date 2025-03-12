0c7b1f37db9f71897e2256a04349b2d0993a04d5
Paul Battley <pbattley@gmail.com>
2025-03-12 16:50:09 +0000

    Remove all references to database from app
    This is a non-transactional service and does not store any data itself.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -2,8 +2,6 @@
 require "spec_helper"
 ENV["RAILS_ENV"] ||= "test"
 require_relative "../config/environment"
-# Prevent database truncation if the environment is production
-abort("The Rails environment is running in production mode!") if Rails.env.production?
 # Uncomment the line below in case you have `--require rails_helper` in the `.rspec` file
 # that will avoid rails generators crashing because migrations haven't been run yet
 # return unless Rails.env.test?
@@ -25,11 +23,4 @@ require "rspec/rails"
 #
 Rails.root.glob("spec/support/**/*.rb").sort_by(&:to_s).each { |f| require f }
 
-# Checks for pending migrations and applies them before tests are run.
-# If you are not using ActiveRecord, you can remove these lines.
-# begin
-#  ActiveRecord::Migration.maintain_test_schema!
-# rescue ActiveRecord::PendingMigrationError => e
-#  abort e.to_s.strip
-# end
 RSpec.configure(&:filter_rails_from_backtrace!)
