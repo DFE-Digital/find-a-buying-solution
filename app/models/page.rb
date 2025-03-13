2b4a3a678857eceee866e7052421fc06a066a988
hrmtl <hrishi.mittal@education.gov.uk>
2025-03-13 21:39:43 +0000

    Add Page model and specs

---
+++ app/models/page.rb
@@ -0,0 +1,25 @@
+class Page
+  include ActiveModel::Model
+
+  attr_reader :id, :title, :body, :description, :slug, :sidebar, :breadcrumbs
+
+  def initialize(entry)
+    @id = entry.id
+    @title = entry.fields[:title]
+    @body = entry.fields[:body]
+    @description = entry.fields[:description]
+    @slug = entry.fields[:slug]
+    @sidebar = entry.fields[:sidebar]
+    @breadcrumbs = entry.fields[:breadcrumbs]
+  end
+
+  def self.find_by_slug!(slug)
+    entry = ContentfulClient.entries(
+      content_type: "page",
+      'fields.slug': slug
+    ).first
+    raise ContentfulRecordNotFoundError, "Page: '#{slug}' not found" unless entry
+
+    new(entry)
+  end
+end
