03f4a676b9a3a4abaf78a2042bc236390570d0cf
hrmtl <hrishi.mittal@education.gov.uk>
2025-03-13 21:30:16 +0000

    Add helper to check url safety

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,13 @@
 module ApplicationHelper
   include MarkdownHelper
   include SvgHelper
+
+  def safe_url(url)
+    return "#" unless url.is_a?(String)
+
+    uri = URI.parse(url)
+    uri.is_a?(URI::HTTP) || uri.is_a?(URI::HTTPS) ? url : "#"
+  rescue URI::InvalidURIError
+    "#"
+  end
 end
