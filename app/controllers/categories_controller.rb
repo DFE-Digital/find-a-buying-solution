d4b5adc354be7731a2a9d48e391c3b00f638f1c0
Bhavana <bhavana.kaushik@gmail.com>
2025-03-15 20:25:57 +0000

    - Added View all frameworks page

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,5 @@
 class CategoriesController < ApplicationController
-  before_action :enable_search_in_header, except: :index
+  before_action :enable_search_in_header, except: [:index, :all]
 
   def index
     @categories = Category.all
@@ -11,5 +11,10 @@ class CategoriesController < ApplicationController
     @subcategories = @category.subcategories
     @selected_subcategories = @subcategories.select { params[:subcategory_slugs]&.include?(it.slug) }
     @solutions = @category.filtered_solutions(subcategory_slugs: params[:subcategory_slugs]&.compact_blank)
+  end
+
+  def all
+    @categories = Category.all
+    render layout: "all_frameworks"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root "categories#index"
+  get "/categories/all", to:"categories#all"
   resources :categories, only: %i[show index], param: :slug
   resources :solutions, only: %i[show], param: :slug
   get "/search", to: "search#index"
