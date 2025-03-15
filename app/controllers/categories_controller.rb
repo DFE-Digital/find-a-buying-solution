ae973bd44423ad705460e605c97bb3a0fe306e78
Bhavana <bhavana.kaushik@gmail.com>
2025-03-15 23:23:25 +0000

    - corrected typo

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,5 @@
 class CategoriesController < ApplicationController
-  before_action :enable_search_in_header, except: %i[index sall]
+  before_action :enable_search_in_header, except: %i[index all]
 
   def index
     @categories = Category.all
