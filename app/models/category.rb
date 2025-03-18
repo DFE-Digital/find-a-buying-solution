29ab9a0f5fd1524d06d7e7da0606d86cb5fe581f
hrmtl <hrishi.mittal@education.gov.uk>
2025-03-18 07:22:01 +0000

    Replace use of category summary with description.

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,12 +1,11 @@
 class Category
   include ActiveModel::Model
 
-  attr_reader :id, :title, :summary, :description, :slug, :solutions, :subcategories
+  attr_reader :id, :title, :description, :slug, :solutions, :subcategories
 
   def initialize(entry)
     @id = entry.id
     @title = entry.fields[:title]
-    @summary = entry.fields[:summary]
     @description = entry.fields[:description]
     @slug = entry.fields[:slug]
     @solutions = entry.fields.fetch(:solutions, []).map { Solution.new(it) }.sort_by(&:title)
@@ -16,7 +15,7 @@ class Category
   def self.all
     ContentfulClient.entries(
       content_type: "category",
-      select: "sys.id,fields.title,fields.summary,fields.slug",
+      select: "sys.id,fields.title,fields.description,fields.slug",
       order: "fields.title"
     ).map { new(it) }
   end
@@ -25,7 +24,7 @@ class Category
     ContentfulClient.entries(
       content_type: "category",
       query: query,
-      select: "sys.id,fields.title,fields.summary,fields.slug"
+      select: "sys.id,fields.title,fields.description,fields.slug"
     ).map { new(it) }
   end
 
@@ -38,7 +37,7 @@ class Category
       content_type: "category",
       'fields.slug': slug,
       include: 1,
-      select: "sys.id,fields.title,fields.summary,fields.description,fields.slug,fields.subcategories,fields.solutions"
+      select: "sys.id,fields.title,fields.description,fields.slug,fields.subcategories,fields.solutions"
     ).first
     raise ContentfulRecordNotFoundError, "Category: '#{slug}' not found" unless entry
 
