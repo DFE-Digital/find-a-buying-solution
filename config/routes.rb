baeae8dd994223e3173fb4052d670a129a862387
hrmtl <hrishi.mittal@education.gov.uk>
2025-03-18 08:06:56 +0000

    Fix search route. Add request specs.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   root "categories#index"
   resources :categories, only: %i[show index], param: :slug
   resources :solutions, only: %i[show], param: :slug
-  get "/:slug", to: "pages#show", as: :page
   get "/search", to: "search#index"
+  get "/:slug", to: "pages#show", as: :page
 end
