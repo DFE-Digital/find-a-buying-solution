4915aad73246892b172fb550e66b9d398fefcdbf
hrmtl <hrishi.mittal@education.gov.uk>
2025-03-27 06:42:04 +0000

    Update spec failure message

--- spec/system/error_pages_spec.rb
+++ spec/system/error_pages_spec.rb
@@ -19,7 +19,7 @@ RSpec.describe "Error pages" do
     error_pages.each do |error_code, content|
       error_page_footer = content.match(/<footer.*?<\/footer>/m).to_s.squish
 
-      expect(error_page_footer).to eq(rendered_footer), "#{error_code}.html footer differs from application footer partial"
+      expect(error_page_footer).to eq(rendered_footer), "#{error_code}.html footer differs from application footer partial. Run `rake footer:update_static_pages` to update it automatically."
     end
   end
 end
