668f6fdbced56dffd6f05d77cecfcc7396cbf27c
hrmtl <hrishi.mittal@education.gov.uk>
2025-03-27 15:55:23 +0000

    Add support for custom call to action text on Solution website link

--- app/models/solution.rb
+++ app/models/solution.rb
@@ -2,7 +2,7 @@ class Solution
   include ActiveModel::Model
   include HasRelatedContent
 
-  attr_reader :id, :title, :description, :summary, :slug, :provider_name, :url, :categories, :subcategories
+  attr_reader :id, :title, :description, :summary, :slug, :provider_name, :url, :categories, :subcategories, :call_to_action
 
   def initialize(entry)
     @id = entry.id
@@ -12,6 +12,7 @@ class Solution
     @slug = entry.fields[:slug]
     @provider_name = entry.fields[:provider_name]
     @url = entry.fields[:url]
+    @call_to_action = entry.fields[:call_to_action]
     @categories = entry.fields[:categories]
     @subcategories = entry.fields[:subcategories]
     super
@@ -48,6 +49,7 @@ class Solution
         fields.summary
         fields.slug
         fields.provider_name
+        fields.call_to_action
         fields.url
       ].join(",")
     ).find { |solution| solution.fields[:slug] == slug }
