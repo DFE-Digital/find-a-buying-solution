aa2b52f74c46ffcc89f378d3bd2fe72daca4efac
Bhavana <bhavana.kaushik@gmail.com>
2025-04-22 10:38:58 +0100

    - Access suffix only when present

--- app/models/solution.rb
+++ app/models/solution.rb
@@ -2,7 +2,7 @@ class Solution
   include ActiveModel::Model
   include HasRelatedContent
 
-  attr_reader :id, :title, :description, :expiry, :summary, :slug, :provider_name, :url, :categories, :subcategories, :call_to_action
+  attr_reader :id, :title, :description, :expiry, :summary, :slug, :provider_name, :url, :categories, :subcategories, :suffix, :call_to_action
 
   def initialize(entry)
     @id = entry.id
@@ -16,13 +16,14 @@ class Solution
     @call_to_action = entry.fields[:call_to_action]
     @categories = entry.fields[:categories]
     @subcategories = entry.fields[:subcategories]
+    @suffix = entry.fields[:suffix]
     super
   end
 
   def self.all(category_id: nil)
     params = {
       content_type: "solution",
-      select: "sys.id, fields.title, fields.description, fields.expiry, fields.slug, fields.categories, fields.subcategories",
+      select: "sys.id, fields.title, fields.description, fields.expiry, fields.slug, fields.categories, fields.subcategories, fields.suffix",
       order: "fields.title",
       "fields.categories.sys.id[in]": category_id,
     }.compact
@@ -45,14 +46,15 @@ class Solution
       select: %w[
         sys.id
         fields.title
-        fields.description
-        fields.expiry
-        fields.related_content
         fields.summary
+        fields.description
         fields.slug
         fields.provider_name
-        fields.call_to_action
         fields.url
+        fields.expiry
+        fields.call_to_action
+        fields.suffix
+        fields.related_content
       ].join(",")
     ).find { |solution| solution.fields[:slug] == slug }
     raise ContentfulRecordNotFoundError unless entry
