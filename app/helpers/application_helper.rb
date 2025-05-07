4fa864eb64c9a3d899898357701dc6e0ab76b313
hrmtl <hrishi.mittal@education.gov.uk>
2025-05-07 12:59:15 +0100

    Add link to Usability Survey on Solution show page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,6 +12,18 @@ module ApplicationHelper
     "#"
   end
 
+  def usability_survey_url(url)
+    base_url = "https://get-help-buying-for-schools.service.gov.uk/usability_surveys/new"
+    safe_url = safe_url(url)
+    return_url = safe_url == "#" ? request.original_url : safe_url
+
+    params = {
+      service: "find_a_buying_solution",
+      return_url: UrlVerifier.generate(return_url),
+    }
+    "#{base_url}?#{params.to_query}"
+  end
+
   def format_date(date_string)
     return "" if date_string.blank?
 
