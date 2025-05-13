096b3c7b5829fcb97fac18dfcd692123b5e8ecb9
hrmtl <hrishi.mittal@education.gov.uk>
2025-05-13 13:36:23 +0100

    Add search validation

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,15 +1,40 @@
 class SearchController < ApplicationController
+  # These are the limits for the Contentful search API
+  MAX_QUERY_LENGTH = 2000
+  MAX_WORDS = 25
+
   before_action :disable_search_in_header
 
   def index
-    @solutions = Solution.search(query: params[:query])
-    @categories = Category.search(query: params[:query])
-    @show_search_in_header = false
+    query = params[:query].strip
+    @solutions = []
+    @categories = []
+    unless invalid_query?(query)
+      @solutions = Solution.search(query: query)
+      @categories = Category.search(query: query)
+    end
+  rescue Contentful::BadRequest
+    @validation_error = :contentful_error
   end
 
 private
 
   def disable_search_in_header
     @show_search_in_header = false
+  end
+
+  def invalid_query?(query)
+    if query.empty?
+      @validation_error = :empty
+      true
+    elsif query.length > MAX_QUERY_LENGTH
+      @validation_error = :too_long
+      true
+    elsif query.split(/\s+/).size > MAX_WORDS
+      @validation_error = :too_many_words
+      true
+    else
+      false
+    end
   end
 end
