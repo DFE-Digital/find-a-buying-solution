325332f301d8a94a99625983a14a1dc01b8f01bc
hrmtl <hrishi.mittal@education.gov.uk>
2025-05-27 15:30:18 +0100

    Update hosts list

--- config/application.rb
+++ config/application.rb
@@ -28,7 +28,8 @@ module FindABuyingSolution
     config.middleware.use RealIp
     config.middleware.use FafDomainRedirect
 
-    config.hosts << ENV["FAF_DOMAIN"] if ENV["FAF_DOMAIN"].present?
+    heroku_domain = "#{ENV['HEROKU_APP_NAME']}.herokuapp.com" if ENV["HEROKU_APP_NAME"].present?
+    config.hosts = [heroku_domain, ENV["APP_DOMAIN"], ENV["FAF_DOMAIN"]].compact
 
     # Configuration for the application, engines, and railties goes here.
     #
