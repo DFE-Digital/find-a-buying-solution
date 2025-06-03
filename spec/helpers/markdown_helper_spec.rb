3d3fc43e1fcaf46ddc52508039c8cde2a6ffeb70
hrmtl <hrishi.mittal@education.gov.uk>
2025-06-03 16:33:24 +0100

    Add external link attributes to Markdown output

---
+++ spec/helpers/markdown_helper_spec.rb
@@ -0,0 +1,13 @@
+require "rails_helper"
+
+RSpec.describe MarkdownHelper, type: :helper do
+  describe "#render_markdown_to_html" do
+    it "adds external link attributes to links" do
+      markdown = "[Internal](/internal) [External](https://example.com)"
+      html = helper.render_markdown_to_html(markdown)
+
+      expect(html).to include('<a href="/internal">Internal</a>')
+      expect(html).to include('<a href="https://example.com" target="_blank" rel="noopener noreferrer">External</a>')
+    end
+  end
+end
