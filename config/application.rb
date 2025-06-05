2899ae6ee7b0aa6f72309bc38dfd5bc20ecca3c5
Bhavana <bhavana.kaushik@gmail.com>
2025-06-05 12:01:16 +0100

    Redirect HTTP requests to HTTPS - added middleware

--- config/application.rb
+++ config/application.rb
@@ -24,7 +24,9 @@ module FindABuyingSolution
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w[assets tasks])
+    config.autoload_paths += %W(#{config.root}/lib)
 
+    config.middleware.use "ForceSSLHeroku"
     config.middleware.use RealIp
     config.middleware.use FafDomainRedirect
 
---
+++ lib/middleware/force_ssl_heroku.rb
@@ -0,0 +1,25 @@
+class ForceSSLHeroku
+  def initialize(app)
+    @app = app
+  end
+
+  def call(env)
+    request = Rack::Request.new(env)
+
+    if insecure_request?(request)
+      redirect_to_ssl(request)
+    else
+      @app.call(env)
+    end
+  end
+
+  private
+
+  def insecure_request?(request)
+    request.env["HTTP_X_FORWARDED_PROTO"] == "http"
+  end
+
+  def redirect_to_ssl(request)
+    [301, { "Location" => request.url.sub(/^http:/, "https:") }, []]
+  end
+end
