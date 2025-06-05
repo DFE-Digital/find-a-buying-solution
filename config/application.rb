bcb83b2356070f49dc32a8c7835acfb4e9d1fafe
Bhavana <bhavana.kaushik@gmail.com>
2025-06-05 12:01:16 +0100

    Added a new middleware to handle and enfore http-https redirects only for production environment

--- config/application.rb
+++ config/application.rb
@@ -1,4 +1,5 @@
 require_relative "boot"
+require_relative "../lib/force_ssl"
 require_relative "../lib/real_ip"
 require_relative "../lib/faf_domain_redirect"
 
@@ -25,6 +26,7 @@ module FindABuyingSolution
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w[assets tasks])
 
+    config.middleware.use ForceSsl
     config.middleware.use RealIp
     config.middleware.use FafDomainRedirect
 
