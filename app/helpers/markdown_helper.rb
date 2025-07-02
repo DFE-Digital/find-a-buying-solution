62964bd2fdbeab9aec8b3486ea6b3d835fc31a8a
Tony McKernan <tmckernan@contractor.indeed.com>
2025-07-02 16:07:52 +0100

    FB-342 update Typography

--- app/helpers/markdown_helper.rb
+++ app/helpers/markdown_helper.rb
@@ -3,15 +3,30 @@ module MarkdownHelper
     return "" if markdown_content.blank?
 
     html = Kramdown::Document.new(markdown_content).to_html
-    return html.html_safe unless html.include?("href")
 
     doc = Nokogiri::HTML.fragment(html)
+    doc = update_class(doc, "h2", "govuk-heading-s")
+    doc = update_class(doc, "p", "govuk-body")
+
+    return doc.to_html.html_safe unless html.include?("href")
+
     doc.css("a[href]").each do |link|
       external_link_attributes(link["href"]).each do |key, value|
         link[key] = value
+        link["class"] = [link["class"], "govuk-body-s"].compact.join(" ")
       end
     end
 
     doc.to_html.html_safe
+  end
+
+  def update_class(doc, tag_type, class_name)
+    doc.css(tag_type).each do |tag|
+      unless tag["class"] && tag["class"].include?(class_name)
+        tag["class"] = [tag["class"], class_name].compact.join(" ")
+      end
+    end
+
+    doc
   end
 end
--- spec/helpers/markdown_helper_spec.rb
+++ spec/helpers/markdown_helper_spec.rb
@@ -5,9 +5,8 @@ RSpec.describe MarkdownHelper, type: :helper do
     it "adds external link attributes to links" do
       markdown = "[Internal](/internal) [External](https://example.com)"
       html = helper.render_markdown_to_html(markdown)
-
       expect(html).to include('<a href="/internal">Internal</a>')
-      expect(html).to include('<a href="https://example.com" target="_blank" rel="noopener noreferrer">External</a>')
+      expect(html).to include('<a href="https://example.com" target="_blank" class="govuk-body-s govuk-body-s" rel="noopener noreferrer">External</a>')
     end
   end
 end
