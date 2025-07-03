365ce28156283906d142ca0be2019fcbbb1e31fa
Tony McKernan <tmckernan@contractor.indeed.com>
2025-07-03 16:30:08 +0100

    add a short-circuit to return html if the document does not have a h1, p or href tags

--- app/helpers/markdown_helper.rb
+++ app/helpers/markdown_helper.rb
@@ -4,11 +4,12 @@ module MarkdownHelper
 
     html = Kramdown::Document.new(markdown_content).to_html
 
-    doc = Nokogiri::HTML.fragment(html)
-    doc = update_class(doc, "h2", "govuk-heading-s")
-    doc = update_class(doc, "p", "govuk-body")
+    return html.html_safe unless html.match?(/href|<p\b|<h2\b/)
 
-    return doc.to_html.html_safe unless html.include?("href")
+    doc = Nokogiri::HTML.fragment(html)
+
+    doc = update_class(doc, "h2", "govuk-heading-m")
+    doc = update_class(doc, "p", "govuk-body")
 
     doc.css("a[href]").each do |link|
       external_link_attributes(link["href"]).each do |key, value|
@@ -21,6 +22,8 @@ module MarkdownHelper
   end
 
   def update_class(doc, tag_type, class_name)
+    return doc unless doc.at_css(tag_type)
+
     doc.css(tag_type).each do |tag|
       unless tag["class"] && tag["class"].include?(class_name)
         tag["class"] = [tag["class"], class_name].compact.join(" ")
--- spec/helpers/markdown_helper_spec.rb
+++ spec/helpers/markdown_helper_spec.rb
@@ -8,5 +8,18 @@ RSpec.describe MarkdownHelper, type: :helper do
       expect(html).to include('<a href="/internal" class="govuk-body-s">Internal</a>')
       expect(html).to include('<a href="https://example.com" target="_blank" rel="noopener noreferrer" class="govuk-body-s">External</a>')
     end
+
+    it "add the correct css class for h2 and paragraph tags" do
+      markdown = "## This a heading\nThis is a random paragraph"
+      html = helper.render_markdown_to_html(markdown)
+      expect(html).to include('<h2 id="this-a-heading" class="govuk-heading-s">This a heading</h2>')
+      expect(html).to include('<p class="govuk-body">This is a random paragraph</p>')
+    end
+
+    it "add no css class for h1 heading" do
+      markdown = "# H1 heading"
+      html = helper.render_markdown_to_html(markdown)
+      expect(html).to include('<h1 id="h1-heading">H1 heading</h1>')
+    end
   end
 end
