9689c0b1b05a482659ab1b6c4e35a84088f94de8
Tony McKernan <tmckernan@contractor.indeed.com>
2025-07-24 14:56:58 +0100

    FB-322 default category as breadcrumbe name when coming via a category page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root "categories#index"
-  resources :categories, only: %i[show index], param: :slug
+  resources :categories, only: %i[show index], param: :slug do
+    resources :solutions, only: %i[show index], param: :slug
+  end
+
   resources :solutions, only: %i[show index], param: :slug
   get "/search", to: "search#index"
   post "/events", to: "events#create"
