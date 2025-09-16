249cb0f035143a90bb295fdca8e2e410c8faba13
hrmtl <hrishi.mittal@education.gov.uk>
2025-09-16 10:19:02 +0100

    Update offers page and specs

--- spec/requests/offers_spec.rb
+++ spec/requests/offers_spec.rb
@@ -19,7 +19,11 @@ RSpec.describe "Offers", :vcr, type: :request do
     end
 
     it "displays offers list" do
-      expect(response.body).to include("provider-expires chevron-card-list")
+      expect(response.body).to include("chevron-card-list")
+    end
+
+    it "displays offer titles and links" do
+      expect(response.body).to have_link("High interest bank accounts")
     end
   end
 end
